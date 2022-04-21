f3b167f195824032c4afc5fc7e6f452e5d2f313b
Vasya <vskesha@gmail.com>
2022-04-21 18:16:25 +0300

    зміни зроблені через термінал

--- Hello.py
+++ Hello.py
@@ -2,3 +2,5 @@ string = input("Enter your name: ")
 age = input("Enter your age: ")
 print("Hello {}. Are you {}?.".format(string, age))
 print(f"Hello {string}. Are you really {age}?")
+ptint("This print was added in new branch Bra2 in Terminal")
+
